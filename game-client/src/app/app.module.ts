9e26228b9b92976eea9714071981776472b03fc5
Wpooley <william.pooley@sunoco.com>
2017-05-08 21:37:57 -0500

    adding changes to pull

--- game-client/src/app/app.module.ts
+++ game-client/src/app/app.module.ts
@@ -20,24 +20,3 @@ import { AppComponent } from './app.component';
 })
 export class AppModule { }
 
-
-
-import { NgModule }           from '@angular/core';
-import { BrowserModule }      from '@angular/platform-browser';
-/* App Root */
-import { AppComponent }       from './app.component.3';
-
-
-/* Routing Module */
-import { AppRoutingModule }   from './app-routing.module.3';
-@NgModule({
-  imports:      [
-    BrowserModule,
-    ContactModule,
-    AppRoutingModule
-  ],
-  providers:    [ UserService ],
-  declarations: [ AppComponent, HighlightDirective, TitleComponent ],
-  bootstrap:    [ AppComponent ]
-})
-export class AppModule { }
