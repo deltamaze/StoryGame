c6ec57d10b5c1728d59ac12fe7083d2be701acf0
deltamaze <wcpooley@gmail.com>
2017-08-12 22:40:05 -0500

    finished create game form with reactive

--- game-client/src/app/story-game/create-game/create-game.component.ts
+++ game-client/src/app/story-game/create-game/create-game.component.ts
@@ -20,68 +20,30 @@ export class CreateGameComponent implements OnInit {
     this.gameService.getErrorStatus().subscribe(status => this.status = status);
 
     this.createNewGame = this.fb.group({
-      gameName: ['', [Validators.required, Validators.minLength(3),Validators.maxLength(10)]],
-      isPrivate: ['', [Validators.required]],
-      maxPlayers: ['', [Validators.required, Utils.minValue(3),Utils.maxValue(8)]],
-      timeBetweenTurns: ['', [Validators.required, Utils.minValue(5)],Utils.maxValue(60)],
-      totalRounds: ['', [Validators.required,Utils.minValue(5)],Utils.maxValue(30)],
-      startingMessage: ['', [Validators.required, Validators.minLength(5), ,Validators.maxLength(200)]],
+      gameName: [' ', [Validators.required, Validators.minLength(3),Validators.maxLength(10)]],
+      isPrivate: [false, []],
+      maxPlayers: [8, [Validators.required, Utils.minValue(3),Utils.maxValue(8)]],
+      timeBetweenTurns: [30, [Validators.required, Utils.minValue(5),Utils.maxValue(60)]],
+      totalRounds: [15, [Validators.required,Utils.minValue(5),Utils.maxValue(30)]],
+      startingMessage: ['Once upon a time ...', [Validators.required, Validators.minLength(5), ,Validators.maxLength(200)]],
       timeStamp: [null]
     });
   }
 
-  onSubmit({ value, valid }: { value: GameRoom, valid: boolean }) {
-    if(valid)
-    {
-      this.gameService.clearError();
-    }
-    console.log(value, valid);
+  onSubmit({ value }: { value: GameRoom, valid: boolean }) {
+
+    
+    this.gameService.clearError(); //Clear Server Error
+    this.gameService.createGame(value);
   }
-
-
 
   private navHome(): void {
     this.gameService.navHome();
   }
   private createGame(): void {
-    if(this.validateInput())
+    this.gameService.clearError() //Clear Previous Error
       this.gameService.createGame(this.newGame);
   }
-  private validateInput(): boolean {
-    if (!(this.newGame.gameName.length <= 10 && this.newGame.gameName.length >= 3)) //game name longer than 3 characters, and less than 10
-    {
-      this.gameService.handleError("Game Name needs to be between 3 and 10 characters");
-      return false;
-    }
-    else if (!(this.newGame.maxPlayers <= 8 && this.newGame.maxPlayers >= 3))//max players between 3 and 8
-    {
-      this.gameService.handleError("Max Players need to be between 3 and 8 players");
-      return false;
-    }
-    else if (!(this.newGame.timeBetweenTurns <= 60 && this.newGame.timeBetweenTurns >= 5))//turn length between 5 and 60 seconds
-    {
-      this.gameService.handleError("Turn Length must be between 5 and 60 seconds");
-      return false;
-    }
-    else if (!(this.newGame.totalRounds <= 30 && this.newGame.totalRounds >= 5))//max turns between 5 and 30
-    {
-      this.gameService.handleError("Game Rounds must be between 5 and 30 rounds");
-      return false;
-    }
-    else if (!(this.newGame.startingMessage.length <= 200 && this.newGame.startingMessage.length >= 5))//starting sentence between 5 and 200 words
-    {
-      this.gameService.handleError("Starting Sentence needs a length between 5 and 200 characters.");
-      return false;
-    }
-    // else if (this.newGame.password.length > 20)//starting sentence between 5 and 200 words
-    // {
-    //   this.gameService.handleError("Password needs to have a length less than 20 characters.");
-    //   return false;
-    // }
-    else {
-      this.gameService.clearError();
-      return true;
-    }
-  }
+
 
 }
