a5575bb23fb2ccd8a011c4eda8a932de0995954e
deltamaze <wcpooley@gmail.com>
2017-12-24 23:40:32 -0600

    randomize nomination options

--- game-client/src/app/shared/prop-to-array.pipe.ts
+++ game-client/src/app/shared/prop-to-array.pipe.ts
@@ -1,19 +1,20 @@
 
 import { Injectable, Pipe, PipeTransform } from '@angular/core';
 @Pipe({
-   name: 'propToArray'
+    name: 'propToArray'
 })
 @Injectable()
 export class PropToArrayPipe implements PipeTransform {
 
-transform(value, args: string[]): any {
-    const keys = [];
-    for (const key in value) {
-        if (value.hasOwnProperty(key)) {
-            keys.push({key: key, value: value[key]});
+    transform(value, args: string[]): any {
+        const keys = [];
+        for (const key in value) {
+            if (value.hasOwnProperty(key)) {
+                keys.push({ key: key, value: value[key] });
+            }
         }
+        return keys.sort(function (a, b) { return 0.5 - Math.random(); });
     }
-    return keys;
-}}
+}
 
 
