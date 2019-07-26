642266ce207cee26f73dbb9813dff497bd0d28a4
deltamaze <wcpooley@gmail.com>
2019-07-25 23:07:43 -0500

    set username textbox

--- react/src/pages/signin.js
+++ react/src/pages/signin.js
@@ -1,11 +1,23 @@
 import React from 'react';
 import { connect } from 'react-redux';
-
+import { setUsername } from '../services/auth/action';
 
 class SignInPage extends React.Component {
-  // constructor(props) {
-  //   super(props);
-  // }
+  constructor(props) {
+    super(props);
+
+
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleChange(event) {
+    this.props.setUsername(event.target.value);
+  }
+
+  handleSubmit(event) {
+    this.props.setUsername(event.target.value);
+  }
 
   renderConnectingMsg() {
     if (this.props.auth.userToken === '') {
@@ -21,6 +33,13 @@ class SignInPage extends React.Component {
       <div>
         <h1>Login</h1>
         <h1>Current UN: {this.props.auth.username}</h1>
+        <form onSubmit={this.handleSubmit}>
+          <label htmlFor="username">
+          Set Username
+            <input type="text" id="username" value={this.props.auth.username} onChange={this.handleChange} />
+          </label>
+          <input type="submit" value="Submit" />
+        </form>
         <h1>Current Token: {this.props.auth.userToken}</h1>
         {this.renderConnectingMsg()}
       </div>
@@ -32,5 +51,6 @@ class SignInPage extends React.Component {
 export default connect(
   state => ({ auth: state.auth }),
   ({
+    setUsername
   })
 )(SignInPage);
