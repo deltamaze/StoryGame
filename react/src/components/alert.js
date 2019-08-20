7a3e1979c8e6b1deb38d2ab85cd57435bcf62f71
deltamaze <wcpooley@gmail.com>
2019-08-19 23:32:03 -0500

    set up alert component

--- react/src/components/alert.js
+++ react/src/components/alert.js
@@ -5,12 +5,20 @@ import { dismissAlert } from '../services/alerts/action';
 // eslint-disable-next-line react/prefer-stateless-function
 class AlertBanner extends React.Component {
   render() {
-    return (
-      <div>Alert: {this.props.alert.alertMsg}</div>
-    );
+    if (!this.props.alert.dismissed) {
+      return (
+        <div className="alert alert-danger alert-dismissible" role="alert">
+          <h4 className="alert-heading">Error!</h4>
+          <p>Unexpected Error Occured =&gt; {this.props.alert.alertMsg}</p>
+          <button type="button" className="close" aria-label="Close" onClick={this.props.dismissAlert}>
+            <span aria-hidden="true">&times;</span>
+          </button>
+        </div>
+      );
+    }
+    return null; // else condition
   }
 }
-
 export default connect(
   state => ({ alert: state.alert }),
   ({
